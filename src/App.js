13be7fa9eb025c7ecc9740cec2cc4efeb8444d7d
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-11-03 21:33:27 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useRef} from 'react';
 import './App.css';
 
 function App() {
+ 
+  const canvas = useRef(null);
+  
+  const drawSomething = () => {
+    const surface = canvas.current;
+    const draw2D = surface.getContext("2d");
+
+    draw2D.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+    var grd = draw2D.createLinearGradient(window.innerWidth/4, window.innerHeight/4, window.innerWidth/2, window.innerHeight/2);
+    grd.addColorStop(0.5, "blue");
+    grd.addColorStop(.75, "orange");    
+    grd.addColorStop(1, "orangered");
+
+    draw2D.fillStyle = grd;
+    draw2D.shadowBlur = 20;
+    draw2D.shadowColor = "darkslategrey";
+    draw2D.fillRect(window.innerWidth/4, window.innerHeight/4, window.innerWidth/2, window.innerHeight/2);
+    draw2D.strokeRect(window.innerWidth/4, window.innerHeight/4, window.innerWidth/2, window.innerHeight/2);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas
+       ref={canvas} 
+       width={window.innerWidth} 
+       height={window.innerHeight}
+       onClick={()=>drawSomething()}
+       />
     </div>
   );
 }
